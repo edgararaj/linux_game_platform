0eb41ada5ec0a30ed7ec35dcfb5128d63260b386
Edgar Araújo <edgararaj@gmail.com>
2021-07-24 12:47:24 +0100

    better qtcreator script

--- qtcreator-refresh.sh
+++ qtcreator-refresh.sh
@@ -4,4 +4,4 @@ set -e
 
 cd "$(git rev-parse --show-toplevel)"
 
-find * -type f -not \( -name "game_test.*" \) -print > game_test.files
+find * -type f ! -name "game_test.*" -print > game_test.files
